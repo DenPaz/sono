f8b6864be794341451838f3b4af4fa839dcf7c7e
Dennis Paz <dppazlopez@gmail.com>
2026-04-03 10:19:59 -0300

    enable minification in build configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: { alias: { "@": path.resolve(__dirname, "./static/js") } },
   base: "/static/",
   build: {
+    minify: true,
     manifest: true,
     outDir: path.resolve(__dirname, "./static/dist"),
     emptyOutDir: true,
