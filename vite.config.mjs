ede51fd034bf7d2c26c1d59922d37348790e16a3
Dennis Paz <dppazlopez@gmail.com>
2026-04-03 10:25:59 -0300

    update build configuration to allow minification control via environment variable

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: { alias: { "@": path.resolve(__dirname, "./static/js") } },
   base: "/static/",
   build: {
-    minify: true,
+    minify: process.env.DJANGO_VITE_BUILD_MINIFY || true,
     manifest: true,
     outDir: path.resolve(__dirname, "./static/dist"),
     emptyOutDir: true,
