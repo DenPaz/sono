c4ef54cb9ac88eaeeec4e169330adbf101a20c27
Dennis Paz <dppazlopez@gmail.com>
2026-05-02 19:29:34 -0300

    feat: update views and urls to include ParentListView and enhance UserListView with profile fetching

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,3 +1,19 @@
+from django.urls import path
+
+from .views import ParentListView
+from .views import UserListView
+
 app_name = "users"
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        route="",
+        view=UserListView.as_view(),
+        name="user_list",
+    ),
+    path(
+        route="parents/",
+        view=ParentListView.as_view(),
+        name="parent_list",
+    ),
+]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,15 @@
+from django.utils.translation import gettext as _
 from django.views import View
 from django.views.i18n import set_language
+from django_filters.views import FilterView
+
+from apps.core.viewmixins import AllowedRolesMixin
+from apps.core.viewmixins import HtmxTemplateMixin
+
+from .enums import UserRole
+from .filters import UserFilter
+from .models import Parent
+from .models import User
 
 
 class SetLanguageView(View):
@@ -10,3 +20,26 @@ class SetLanguageView(View):
             profile.language = language
             profile.save(update_fields=["language", "modified"])
         return set_language(request)
+
+
+class UserListView(AllowedRolesMixin, HtmxTemplateMixin, FilterView):
+    model = User
+    filterset_class = UserFilter
+    allowed_roles = [UserRole.ADMIN]
+    template_name = "users/user_list.html"
+    htmx_template_name = "#user-table"
+    context_object_name = "users"
+    extra_context = {"page_title": _("Users")}
+    paginate_by = 20
+
+    def get_queryset(self):
+        return super().get_queryset().with_profile()
+
+
+class ParentListView(UserListView):
+    model = Parent
+    allowed_roles = [UserRole.ADMIN, UserRole.SPECIALIST]
+    template_name = "users/parent_list.html"
+    htmx_template_name = "#parent-table"
+    context_object_name = "parents"
+    extra_context = {"page_title": _("Parents")}
