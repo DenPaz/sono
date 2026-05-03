d671b416e9278bfe3c01b76af793cfa5932ebeef
Dennis Paz <dppazlopez@gmail.com>
2026-05-03 00:28:56 -0300

    feat: implement role-based template rendering in IndexView and add corresponding templates

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,16 @@
 from django.views.generic import TemplateView
 
+from apps.users.enums import UserRole
+
 
 class IndexView(TemplateView):
     template_name = "dashboard/index.html"
+
+    def get_template_names(self):
+        role = self.request.user.role
+        template_name = {
+            UserRole.ADMIN: "dashboard/admin.html",
+            UserRole.SPECIALIST: "dashboard/specialist.html",
+            UserRole.PARENT: "dashboard/parent.html",
+        }.get(role, self.template_name)
+        return [template_name]
